be07040f5a377a3d90d14aa2c05d396e85b71aeb
Matt Thomas <matt@betweenbrain.com>
2013-05-09 21:34:26 -0400

    Initial working version

---
+++ tmpl/default.php
@@ -0,0 +1,17 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       default.php
+ * Created    5/9/13 8:35 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+?>
+<div class="jfontsize">
+	<a class="jfontsize-button" id="jfontsize-minus">A-</a>
+	<a class="jfontsize-button" id="jfontsize-default">A</a>
+	<a class="jfontsize-button" id="jfontsize-plus">A+</a>
+</div>
